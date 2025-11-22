a99b6eee0b972522722d710c9f6d583c7eda65df
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-22 20:04:11 +0530

    Currency Convertor

--- Introduction-to-Programming/CurrencyConverter.java
+++ Introduction-to-Programming/CurrencyConverter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CurrencyConverter {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
 
         System.out.print("Enter amount in INR: ");
         float inr = scanner.nextFloat();
