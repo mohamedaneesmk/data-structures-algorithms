ef3e92b754239cd9f60c1712cfdc6d6c4f0e7dea
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-22 20:16:25 +0530

    Perimeter Of Circle

---
+++ Conditionals-Loops/PerimeterOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PerimeterOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        double perimeter = 2 * Math.PI * r;
+        System.out.printf("Perimeter: %.2f", perimeter);
+        sc.close();
+    }
+}
