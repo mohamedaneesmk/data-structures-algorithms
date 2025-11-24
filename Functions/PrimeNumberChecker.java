3457187e883cf9d7d310a84d4a15d2acbcaea5ce
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-24 11:22:10 +0530

    Prime Number Checker

--- Functions/PrimeNumberChecker.java
+++ Functions/PrimeNumberChecker.java
@@ -2,7 +2,7 @@ package Functions;
 
 import java.util.Scanner;
 
-public class PrimeNumberChecker {
+public class PrimeNumberChecker { 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
