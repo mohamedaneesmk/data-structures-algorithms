66081a08391ce3c151fb846b58b2326211325898
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-30 21:58:39 +0530

    Shuffle The Array

--- Arrays-ArrayList/Problems/ShuffleTheArray.java
+++ Arrays-ArrayList/Problems/ShuffleTheArray.java
@@ -13,7 +13,7 @@ public class ShuffleTheArray {
     private static int[] findShuffledArray(int[] nums, int n) {
         int[] result = new int[n * 2];
 
-        for(int i = 0;i < n; i++){
+        for(int i = 0; i < n; i++){
             result[2 * i] = nums[i];
             result[2 * i + 1] = nums[n + i];
         }
