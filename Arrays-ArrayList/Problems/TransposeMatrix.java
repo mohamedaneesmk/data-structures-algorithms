c948065323d59b9f2bad57a0aff4bc73c83bcd9b
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-02 16:01:41 +0530

    Transpose Matrix

---
+++ Arrays-ArrayList/Problems/TransposeMatrix.java
@@ -0,0 +1,29 @@
+package Problems;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        transposeMatrix(matrix);
+    }
+
+    private static void transposeMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = row; col < matrix[0].length; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
