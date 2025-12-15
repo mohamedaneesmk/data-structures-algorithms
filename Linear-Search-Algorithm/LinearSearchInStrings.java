e3b8911885fde1c5c27ba9bdfe3661213c79e562
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-15 13:49:31 +0530

    Linear Search In Strings

--- Linear-Search-Algorithm/LinearSearchInStrings.java
+++ Linear-Search-Algorithm/LinearSearchInStrings.java
@@ -1,13 +1,13 @@
 public class LinearSearchInStrings {
     public static void main(String[] args) {
         String name = "Anees";
-        char target = 'y';
+        char target = 'y'; 
         System.out.println(isCharacterFound(name, target));
     }
 
     private static boolean isCharacterFound(String name, char target) {
         if(name.length() == 0) return false;
-        
+
         for (char ch : name.toCharArray()) {
             if (ch == target) {
                 return true;
