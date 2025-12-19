574df358363ede759750e62acb02da18175b75b5
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-19 10:28:55 +0530

    Two Sum II - Input Array is Sorted LeetCode #167

---
+++ Linear-Search-Algorithm/Problems/TwoSumII.java
@@ -0,0 +1,30 @@
+public class TwoSumII {
+    public static void main(String[] args) {
+        int[] numbers = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = findTheTwoSum(numbers, target);
+
+        for (int num : result) {
+            System.out.print(num+" ");
+        }
+
+    }
+
+    private static int[] findTheTwoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] {};
+    }
+}
