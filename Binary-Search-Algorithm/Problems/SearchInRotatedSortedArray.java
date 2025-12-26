30f743ee9caf45876ce8ac362cf912c9bf53b08f
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-26 09:49:13 +0530

    Search In Rotated Sorted Array | LeetCode #33

--- Binary-Search-Algorithm/Problems/SearchInRotatedSortedArray.java
+++ Binary-Search-Algorithm/Problems/SearchInRotatedSortedArray.java
@@ -1,20 +1,21 @@
 public class SearchInRotatedSortedArray {
+
     public static void main(String[] args) {
-        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
         int target = 0;
 
         int result = findSearchInRotatedSortedArray(arr, target);
-        System.out.println(result);
+        System.out.println(result); // Output: 4
     }
 
     private static int findSearchInRotatedSortedArray(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
 
-        while (start < end) {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (target == nums[mid]) {
+            if (nums[mid] == target) {
                 return mid;
             }
 
@@ -23,17 +24,17 @@ public class SearchInRotatedSortedArray {
                 if (target >= nums[start] && target < nums[mid]) {
                     end = mid - 1;
                 } else {
-                    start = mid - 1;
+                    start = mid + 1;
                 }
             }
-
             // Right half is sorted
-            if (target <= nums[start] && target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
-
         }
 
         return -1;
