a1e0276e64c5cb9d1f318ba4e3ef0efadd685e58
mohamedaneesmk <mohamedaneesmk@gmail.com>
2025-12-29 11:35:28 +0530

    Binary Search in Sorted Matrix

--- Binary-Search-Algorithm/2D-Arrays/SortedMatrix.java
+++ Binary-Search-Algorithm/2D-Arrays/SortedMatrix.java
@@ -2,7 +2,7 @@ public class SortedMatrix {
     public static void main(String[] args) {
         int[][] matrix = {
             {1, 3, 5, 7},
-            {10, 11, 16, 20},
+            {10, 11, 16, 20}, 
             {23, 30, 34, 50}
         };
         int target = 3;
