3620e0c329f985447cec4ccf226d14d0187b4195
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-16 21:30:57 +0530

    Selection Sort

--- Selection-Sort-Algorithm/Concepts/SelectionSort.java
+++ Selection-Sort-Algorithm/Concepts/SelectionSort.java
@@ -8,34 +8,6 @@ public class SelectionSort {
         System.out.println(Arrays.toString(nums));
     }
 
-    /*
-     * private static void selectionSort(int[] nums) {
-     * for (int i = 0; i < nums.length; i++) {
-     * int last = nums.length - i - 1;
-     * int maxIndex = getMaxIndex(nums, 0, last);
-     * 
-     * swap(nums, maxIndex, last);
-     * }
-     * }
-     * 
-     * private static void swap(int[] nums, int maxIndex, int last) {
-     * int temp = nums[maxIndex];
-     * nums[maxIndex] = nums[last];
-     * nums[last] = temp;
-     * }
-     * 
-     * private static int getMaxIndex(int[] nums, int start, int end) {
-     * int max = start;
-     * for (int i = start; i <= end; i++) {
-     * if (nums[max] < nums[i]) {
-     * max = i;
-     * }
-     * }
-     * 
-     * return max;
-     * }
-     */ 
-
     public static void selectionSort(int[] arr) {
         int n = arr.length;
 
