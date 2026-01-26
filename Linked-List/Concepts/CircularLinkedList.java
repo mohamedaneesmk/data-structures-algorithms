774e29fe1aa05cc274b6589eab1d3ee9692db695
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-26 19:04:38 +0530

    Circular Linked List

---
+++ Linked-List/Concepts/CircularLinkedList.java
@@ -0,0 +1,47 @@
+class CircularLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node head = null;
+    Node tail = null;
+
+    void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            tail.next = head;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = head;
+        }
+    }
+
+    void display() {
+        if (head == null) return;
+
+        Node temp = head;
+        do {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        } while (temp != head);
+
+        System.out.println("(back to head)");
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedList list = new CircularLinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.display();
+    }
+}
