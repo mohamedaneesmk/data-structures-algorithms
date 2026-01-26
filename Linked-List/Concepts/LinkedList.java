54cb75e16ae124e4eb34241c6a091f683938b84a
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-26 19:02:51 +0530

    Linked List

---
+++ Linked-List/Concepts/LinkedList.java
@@ -0,0 +1,73 @@
+class LinkedList {
+
+    // Node class
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    // Add element at end
+    public void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Delete first occurrence of value
+    public void delete(int value) {
+        if (head == null) return;
+
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null && temp.next.data != value) {
+            temp = temp.next;
+        }
+
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
+
+    // Display list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        list.display();      // 10 -> 20 -> 30 -> null
+
+        list.delete(20);
+        list.display();      // 10 -> 30 -> null
+    }
+}
---
+++ Linked-List/Concepts/Problems/InsertionSortList.java
@@ -0,0 +1,5 @@
+public class InsertionSortList {
+    public static void main(String[] args) {
+        
+    }    
+}
